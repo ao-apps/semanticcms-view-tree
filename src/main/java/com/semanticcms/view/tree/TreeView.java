a3fb397c0bebdbb71fbe19f9d710d589706585da
AO Industries, Inc <support@aoindustries.com>
2021-03-14 11:55:40 -0500

    Separated a base "Any" layer so is more extensible.
    Working at the "Any" level means many type parameters, which can be "<?>".
    This would be used when supporting any different type of document.
    
    Working at the "Document" level means much fewer type parameters
    (pretty much only for current content model context), at the cost of
    being only the "Document" level.
    
    The ao-fluent-html-servlet project now has a parallel concrete implementation.
    This means it also has the single type parameter for current content model
    context.

--- src/main/java/com/semanticcms/view/tree/TreeView.java
+++ src/main/java/com/semanticcms/view/tree/TreeView.java
@@ -22,8 +22,7 @@
  */
 package com.semanticcms.view.tree;
 
-import com.aoindustries.html.FlowContent;
-import com.aoindustries.html.servlet.DocumentEE;
+import com.aoindustries.html.servlet.FlowContent;
 import com.semanticcms.core.model.Page;
 import com.semanticcms.core.model.PageRef;
 import com.semanticcms.core.servlet.SemanticCMS;
@@ -106,7 +105,7 @@ public class TreeView extends View {
 	}
 
 	@Override
-	public <__ extends FlowContent<DocumentEE, __>> void doView(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, __ flow, Page page) throws ServletException, IOException {
+	public <__ extends FlowContent<__>> void doView(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, __ flow, Page page) throws ServletException, IOException {
 		PageRef pageRef = page.getPageRef();
 		flow.h1__(h1 -> h1
 			.text("Page Tree of ").text(page.getTitle())
