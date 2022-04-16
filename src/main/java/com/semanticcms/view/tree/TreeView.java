080a515688a1c55fd489d768437cca6da7f2df8a
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:48 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/view/tree/TreeView.java
+++ src/main/java/com/semanticcms/view/tree/TreeView.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-view-tree - SemanticCMS view of the tree of pages and elements starting at the current page.
- * Copyright (C) 2016, 2017, 2018, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2018, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-view-tree.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.view.tree;
 
 import com.aoapps.html.servlet.FlowContent;
