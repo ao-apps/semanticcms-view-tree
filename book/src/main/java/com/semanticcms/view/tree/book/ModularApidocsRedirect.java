faefeb10c2077871ca75a21a205a706a4839a804
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:51:24 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- book/src/main/java/com/semanticcms/view/tree/book/ModularApidocsRedirect.java
+++ book/src/main/java/com/semanticcms/view/tree/book/ModularApidocsRedirect.java
@@ -36,14 +36,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/view-tree/apidocs/com/*")
 public class ModularApidocsRedirect extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		HttpServletUtil.sendRedirect(
-			HttpServletResponse.SC_MOVED_PERMANENTLY, req, resp,
-			"/view-tree/apidocs/com.semanticcms.view.tree.book/com" + Objects.toString(req.getPathInfo(), ""),
-			URIParametersUtils.of(req.getQueryString()), true, false
-		);
-	}
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    HttpServletUtil.sendRedirect(
+      HttpServletResponse.SC_MOVED_PERMANENTLY, req, resp,
+      "/view-tree/apidocs/com.semanticcms.view.tree.book/com" + Objects.toString(req.getPathInfo(), ""),
+      URIParametersUtils.of(req.getQueryString()), true, false
+    );
+  }
 }
